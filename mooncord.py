8b35576627b6abf889feee883818e7aa73f80d84
eliteSCHW31N <business@eliteschw31n.de>
2022-04-28 08:29:27 +0200

    add klipper rest client

--- mooncord.py
+++ mooncord.py
@@ -10,3 +10,19 @@ import subprocess
 import pathlib
 import traceback
 
+from mc_includes.KlippyWebsocket import KlippyWebsocket
+from mc_includes.KlippyRest import KlippyRest
+
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger('matplotlib').setLevel(logging.WARNING)
+
+# This is here to avoid performance issues opening bed_mesh
+import matplotlib.pyplot  # noqa
+
+class MoonCord:
+    def __init__(self):
+        return
+
+    def connectPrinter(self):
+        self.apiclient = KlippyRest()
+
